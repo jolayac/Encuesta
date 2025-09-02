35da6dbf38624f075694e992cf2ae241c4a0cf51
Juan_Olaya <jolayac@unal.edu.co>
2025-09-02 17:39:57 -0500

    Create Encuesta.py
    Co-Authored-By: jcorredorsa <230039747+jcorredorsa@users.noreply.github.com>
    Co-Authored-By: JPaezGu <230044680+jpaezgu@users.noreply.github.com>

---
+++ Encuesta.py
@@ -0,0 +1,11 @@
+#Juan Sebastián Olaya Castañeda
+#Juan Sebastián Corredor Saenz
+#Juan Camilo Páez Guaspud
+
+#Diseñar una encuesta para 6 users
+#Nombre
+#Carrera
+#Idea de proyecto
+
+User1="as"
+print(User1)
